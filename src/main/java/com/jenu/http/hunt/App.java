bd18d3a9298bf958daa9dc1b5405f9ae4cfcd7f5
janardhanan.s <janardhanan.s@ptpll575.appviewx.com>
2018-06-21 21:18:54 +0530

    id_1 - Initial commit

---
+++ src/main/java/com/jenu/http/hunt/App.java
@@ -0,0 +1,274 @@
+package com.jenu.http.hunt;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.appviewx.common.utils.BeanType;
+import com.appviewx.common.utils.JsonParser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static final String HOST = "https://http-hunt.thoughtworks-labs.net";
+
+	public static final String IN_URL = "/challenge/input";
+
+	public static final String OUT_URL = "/challenge/output";
+
+	public static final String USER_ID = "HJfdkYGgQ";
+
+	public final static void main(String[] args) {
+
+		try {
+
+			final String input = getInput();
+			System.out.println(input);
+
+			final String response = findProductPrice(input);
+			System.out.println(response);
+
+			final String result = postOutput(response);
+			System.out.println(result);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static String findProductPrice(String input) {
+		String responseString = "";
+		try {
+			final List<Product> request = JsonParser.getParser().convertToBean(input, new BeanType<List<Product>>() {
+			});
+			final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			final Date todayDate = new Date();
+
+			Map<String, Integer> response = new HashMap<>();
+			int totalPrice = 0;
+
+			for (Product product : request) {
+				Date historyDate = sdf.parse(product.getStartDate());
+
+				if (product.getEndDate() == null) {
+					if (!historyDate.after(todayDate)) {
+						totalPrice += product.getPrice();
+					}
+				} else {
+					Date futureDate = sdf.parse(product.getEndDate());
+					if (!historyDate.after(todayDate) && !futureDate.before(todayDate)) {
+						totalPrice += product.getPrice();
+					}
+				}
+
+			}
+
+			response.put("totalValue", totalPrice);
+
+			responseString = JsonParser.getParser().convertToJson(response);
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseString;
+	}
+
+	public static String findProductByCategory(String input) {
+		String responseString = "";
+		try {
+			final List<Product> request = JsonParser.getParser().convertToBean(input, new BeanType<List<Product>>() {
+			});
+			final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			final Date todayDate = new Date();
+
+			Map<String, Integer> response = new HashMap<>();
+
+			for (Product product : request) {
+				Date historyDate = sdf.parse(product.getStartDate());
+
+				if (product.getEndDate() == null) {
+					if (!historyDate.after(todayDate)) {
+						Integer k = response.get(product.getCategory());
+						if (k == null) {
+							response.put(product.getCategory(), 1);
+						} else {
+							response.put(product.getCategory(), ++k);
+						}
+
+					}
+				} else {
+					Date futureDate = sdf.parse(product.getEndDate());
+					if (!historyDate.after(todayDate) && !futureDate.before(todayDate)) {
+						Integer k = response.get(product.getCategory());
+						if (k == null) {
+							response.put(product.getCategory(), 1);
+						} else {
+							response.put(product.getCategory(), ++k);
+						}
+
+					}
+				}
+
+			}
+
+			responseString = JsonParser.getParser().convertToJson(response);
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseString;
+	}
+
+	public static String findProductCount(String input) {
+		String responseString = "";
+		try {
+			final List<Product> request = JsonParser.getParser().convertToBean(input, new BeanType<List<Product>>() {
+			});
+			final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			final Date todayDate = new Date();
+			int active = 0;
+
+			Map<String, Integer> response = new HashMap<>();
+
+			for (Product product : request) {
+				Date historyDate = sdf.parse(product.getStartDate());
+
+				if (product.getEndDate() == null) {
+					if (!historyDate.after(todayDate)) {
+						active++;
+					}
+				} else {
+					Date futureDate = sdf.parse(product.getEndDate());
+					if (!historyDate.after(todayDate) && !futureDate.before(todayDate)) {
+						active++;
+					}
+				}
+
+			}
+
+			response.put("count", active);
+
+			responseString = JsonParser.getParser().convertToJson(response);
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseString;
+	}
+
+	public static String processInput(String input) {
+
+		String responseString = "";
+		try {
+			List<Object> request = JsonParser.getParser().convertToBean(input, new BeanType<List<Object>>() {
+			});
+
+			Map<String, Object> response = new HashMap<>();
+			response.put("count", request.size());
+
+			responseString = JsonParser.getParser().convertToJson(response);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseString;
+	}
+
+	public static String getInput() {
+		StringBuilder sb = new StringBuilder();
+		HttpClient httpClient = new DefaultHttpClient();
+		try {
+			HttpGet httpGetRequest = new HttpGet(HOST + IN_URL);
+			httpGetRequest.setHeader("UserID", USER_ID);
+
+			HttpResponse httpResponse = httpClient.execute(httpGetRequest);
+
+			System.out.println("----------------------------------------");
+			System.out.println(httpResponse.getStatusLine());
+			System.out.println("----------------------------------------");
+
+			HttpEntity entity = httpResponse.getEntity();
+
+			byte[] buffer = new byte[1024];
+			if (entity != null) {
+				InputStream inputStream = entity.getContent();
+				try {
+					int bytesRead = 0;
+					BufferedInputStream bis = new BufferedInputStream(inputStream);
+
+					while ((bytesRead = bis.read(buffer)) != -1) {
+						String chunk = new String(buffer, 0, bytesRead);
+						sb.append(chunk);
+						System.out.println(chunk);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						inputStream.close();
+					} catch (Exception ignore) {
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+		return sb.toString();
+	}
+
+	public static String postOutput(String input) {
+		StringBuilder sb = new StringBuilder();
+		HttpClient httpClient = new DefaultHttpClient();
+
+		HttpPost httpPost = new HttpPost(HOST + OUT_URL);
+		httpPost.addHeader("UserID", USER_ID);
+		httpPost.setHeader("Accept", "application/json");
+		httpPost.setHeader("Content-type", "application/json");
+		try {
+
+			HttpEntity entity = new StringEntity(input);
+			httpPost.setEntity(entity);
+
+			HttpResponse response = httpClient.execute(httpPost);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				System.out.println(line);
+				sb.append(line);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+
+		return sb.toString();
+	}
+}
---
+++ src/main/java/com/jenu/http/hunt/Product.java
@@ -0,0 +1,51 @@
+package com.jenu.http.hunt;
+
+public class Product {
+
+	private String name;
+	private String category;
+	private int price;
+	private String startDate;
+	private String endDate;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+}
